814d86bb114342a1163943522605f7fc217fcad4
Lubnnia <lubnnia@gmail.com>
2025-01-10 09:58:59 -0300

    feat: Add expo fonts lib

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components/native';
+
+import theme from '../theme';
+
+declare module 'styled-components' {
+  // CRIAR UMA TIPAGEM BASEADA NO CONTEÚDO DO MEU TEMA
+  type ThemeType = typeof theme;
+
+  /**
+   * EXTENDER DO TEMA PADRÃO DO STYLED COMPONENT
+   * DIZENDO QUAL É O TIPO DO TEMA QUE ESTOU UTILIZANDO
+   * QUAL O CONTEÚDO DO TEMA
+   */
+  export interface DefaultTheme extends ThemeType {}
+}
