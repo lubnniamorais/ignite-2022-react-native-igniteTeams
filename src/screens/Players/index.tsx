d4ea77a8f89faa12fe67a4d09746c75d1f3b7631
lubnniamorais <lubnnia@gmail.com>
2025-02-06 13:39:22 -0300

    feat: Create PlayerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
+import { PlayerCard } from '@components/PlayerCard';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
@@ -11,7 +12,7 @@ import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 export function Players() {
   // Nessse estado iremos armazenar o time que está selecionado
   const [team, setTeam] = useState('TIME A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Lubnnia', 'Lubni']);
 
   return (
     <Container>
@@ -44,6 +45,14 @@ export function Players() {
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => console.log('Removeu')} />
+        )}
+      />
     </Container>
   );
 }
