53e15e45e9475284b1044e0eb486733771aea419
lubnniamorais <lubnnia@gmail.com>
2025-02-14 09:41:21 -0300

    chore: Include useNavigation in Groups screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,6 +3,7 @@ import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { ListEmpty } from '@components/ListEmpty';
+import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
@@ -10,6 +11,12 @@ import { Container } from './styles';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -27,7 +34,7 @@ export function Groups() {
         )}
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }
